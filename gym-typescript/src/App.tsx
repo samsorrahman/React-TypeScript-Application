28bfefbd537ed0efbb25147fd4dd28c3cc9e9df5
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-08-28 23:46:37 -0700

    Benefits section completed

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/scenes/navbar";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
 
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
